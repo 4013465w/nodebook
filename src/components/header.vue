c42b9de687896216528d89a83cc51b4d3a88c543
fddcn <4013465w@sina.com>
2016-03-19 08:27:13 +0800

    demo

--- src/components/header.vue
+++ src/components/header.vue
@@ -4,10 +4,16 @@
   	<a href=""></a>
   	<h1>NodeBook</h1>
   </header>
+    <router-view></router-view>
+	<nav>
+		<a v-link="{ path: '/foo' }">Go to Foo</a>
+    	<a v-link="{ path: '/foo1' }">Go to FOO1</a>
+	</nav>
 </template>
 <style type="text/css">
 	header{
-		color: 
+		background-color: #12FEDF;
+		color: white;
 	}
 </style>
 <script type="text/javascript">
--- src/index.js
+++ src/index.js
@@ -2,21 +2,34 @@ require('./main.css');
 var Vue = require('vue');
 var Router = require('vue-router');
 var Header = require('./components/header.vue');
+var foo = require('./components/foo.vue');
+var foo1 = require('./components/foo1.vue');
+var foo2 = require('./components/foo2.vue');
 Vue.use(Router);
 
 // routingdfds
 var router = new Router();
 
 router.map({
-  '/news/:page': {
-    component: Header
-  }
+  '/foo': {
+    component: foo
+  },
+  '/foo1': {
+    component: foo1
+  },
+  '/foo1/foo2': {
+    component: foo2
+  },
 });
 router.beforeEach(function () {
-  window.scrollTo(0, 0)
+  window.scrollTo(0, 0);
 });
 
-// router.redirect({
-//   '*': '/news/1'
-// });
-router.start(Header, 'body')
+router.redirect({
+  '*': '/foo'
+});
+var div = document.createElement('div');
+div.id='app';
+document.body.insertBefore(div,document.body.childNodes[0])
+
+router.start(Header, '#app')
